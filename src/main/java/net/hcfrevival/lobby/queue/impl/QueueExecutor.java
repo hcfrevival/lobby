86334658fec7a48420ca152532fe5b7e5007eb71
j-rockwell <john@llewkcor.com>
2023-07-14 10:52:50 -0700

    main: deathban check before joining a factions queue

--- src/main/java/net/hcfrevival/lobby/queue/impl/QueueExecutor.java
+++ src/main/java/net/hcfrevival/lobby/queue/impl/QueueExecutor.java
@@ -1,9 +1,13 @@
 package net.hcfrevival.lobby.queue.impl;
 
+import gg.hcfactions.libs.bukkit.scheduler.Scheduler;
+import gg.hcfactions.libs.bukkit.services.impl.deathbans.DeathbanService;
+import gg.hcfactions.libs.bukkit.services.impl.deathbans.impl.Deathban;
 import gg.hcfactions.libs.bukkit.services.impl.items.CustomItemService;
 import gg.hcfactions.libs.bukkit.services.impl.ranks.RankService;
 import gg.hcfactions.libs.bukkit.services.impl.ranks.model.impl.AresRank;
 import gg.hcfactions.libs.bukkit.services.impl.sync.EServerStatus;
+import gg.hcfactions.libs.bukkit.services.impl.sync.EServerType;
 import gg.hcfactions.libs.bukkit.services.impl.sync.impl.SyncServer;
 import lombok.Getter;
 import net.hcfrevival.lobby.LobbyPermissions;
@@ -22,30 +26,49 @@ public record QueueExecutor(@Getter QueueManager manager) implements IQueueExecu
     @Override
     public void enterQueue(Player player, ServerQueue queue) {
         final CustomItemService cis = (CustomItemService) manager.getPlugin().getService(CustomItemService.class);
-        final RankService rankService = (RankService) manager.getPlugin().getService(RankService.class);
+        final RankService rs = (RankService) manager.getPlugin().getService(RankService.class);
+        final DeathbanService ds = (DeathbanService) manager.getPlugin().getService(DeathbanService.class);
 
         if (cis == null) {
             player.sendMessage(ChatColor.RED + "Failed to obtain custom item service");
             return;
         }
 
-        if (rankService == null) {
+        if (rs == null) {
             player.sendMessage(ChatColor.RED + "Failed to obtain rank service");
             return;
         }
 
-        final AresRank rank =  rankService.getHighestRank(player);
-        final ServerQueue currentQueue = manager.getCurrentQueue(player);
-
-        if (currentQueue != null) {
-            currentQueue.remove(player.getUniqueId());
-            player.sendMessage(ChatColor.RED + "You have been removed from your current queue");
+        // TODO: This requires us to have a deathban service set even if we don't need it.
+        if (ds == null) {
+            player.sendMessage(ChatColor.RED + "Failed to obtain deathban service");
+            return;
         }
 
-        queue.add(player.getUniqueId(), rank);
-        cis.getItem(LeaveQueueItem.class).ifPresent(item -> player.getInventory().setItem(4, item.getItem()));
-        player.sendMessage(ChatColor.RESET + "Adding you to the " + queue.getServer().getName() + ChatColor.RESET + " queue...");
-        player.sendMessage(ChatColor.AQUA + "You are now " + ChatColor.YELLOW + "#" + queue.getPosition(player.getUniqueId()) + ChatColor.AQUA + " in queue to join " + queue.getServer().getName());
+        new Scheduler(manager.getPlugin()).async(() -> {
+            final Deathban existingDb = ds.getDeathban(player.getUniqueId());
+
+            new Scheduler(manager.getPlugin()).sync(() -> {
+                if (existingDb != null && !existingDb.isExpired() && queue.getServer().getType().equals(EServerType.FACTIONS)) {
+                    player.sendMessage(ChatColor.RED + "Could not join queue for " + queue.getServer().getName());
+                    player.sendMessage(ds.getDeathbanKickMessage(existingDb));
+                    return;
+                }
+
+                final AresRank rank =  rs.getHighestRank(player);
+                final ServerQueue currentQueue = manager.getCurrentQueue(player);
+
+                if (currentQueue != null) {
+                    currentQueue.remove(player.getUniqueId());
+                    player.sendMessage(ChatColor.RED + "You have been removed from your current queue");
+                }
+
+                queue.add(player.getUniqueId(), rank);
+                cis.getItem(LeaveQueueItem.class).ifPresent(item -> player.getInventory().setItem(4, item.getItem()));
+                player.sendMessage(ChatColor.RESET + "Adding you to the " + queue.getServer().getName() + ChatColor.RESET + " queue...");
+                player.sendMessage(ChatColor.AQUA + "You are now " + ChatColor.YELLOW + "#" + queue.getPosition(player.getUniqueId()) + ChatColor.AQUA + " in queue to join " + queue.getServer().getName());
+            }).run();
+        }).run();
     }
 
     @Override
